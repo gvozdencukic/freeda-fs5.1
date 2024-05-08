836b404cf97e7b4d74375ae76afa73e968b91b02
Gvozden Cukic <gvozdencukic@hotmail.com>
2024-05-08 14:08:52 +0200

    prebacivanje na git posle prvog casa

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'template_parts/header.php'; ?>
+<?php include 'template_parts/main-header.php'; ?>
+<?php include 'template_parts/footer.php'; ?>
+<link rel="stylesheet" href="style.css">
---
+++ template_parts/main-header.php
@@ -0,0 +1,10 @@
+<!-- main header -->
+<header class="mainHeader">
+    <img src="img/coffee-slajd-1.jpg" alt="">
+    <hgroup>
+        <h1>VREME JE ZA KAFICU</h1>
+        <p>Prva pauza na poslu je rezervisana za kaficu</p>
+        <a href="" class="btn btn-yellow">Saznaj više</a>
+    </hgroup>
+
+</header>
