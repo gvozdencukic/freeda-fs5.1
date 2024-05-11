e5db1e0c6517f0686dd97356ea5cb16e1828669b
Gvozden Cukic <gvozdencukic@hotmail.com>
2024-05-11 15:28:50 +0200

    template part ponuda

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
+
 <?php include 'template_parts/header.php'; ?>
+
 <?php include 'template_parts/main-header.php'; ?>
+
+<?php include 'template_parts/part-ponuda.php'; ?>
+
 <?php include 'template_parts/footer.php'; ?>
-<link rel="stylesheet" href="style.css">
---
+++ template_parts/part-ponuda.php
@@ -0,0 +1,28 @@
+<!-- ponuda -->
+<section class="ponuda container py text-center">
+    <h2>ŠTA VAM NUDIMO</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non maiores laboriosam nemo, dolore facere pariatur quas, repellendus voluptates reprehenderit cumque cum, reiciendis dicta delectus accusamus at deserunt adipisci corporis! Unde?</p>
+    <article>
+        <div>
+            <img src="img/coffee1.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos accusantium doloremque minima. Quo aperiam molestias, vitae voluptate accusamus ipsam beatae expedita illum itaque illo perferendis exercitationem fugit. Quod eaque nobis incidunt voluptates unde accusamus et!</p>
+            </div>
+        </div>
+        <div>
+            <img src="img/coffee3.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos accusantium doloremque minima. Quo aperiam molestias, vitae voluptate accusamus ipsam beatae expedita illum itaque illo perferendis exercitationem fugit. Quod eaque nobis incidunt voluptates unde accusamus et!</p>
+            </div>
+        </div>
+        <div>
+            <img src="img/coffee2.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos accusantium doloremque minima. Quo aperiam molestias, vitae voluptate accusamus ipsam beatae expedita illum itaque illo perferendis exercitationem fugit. Quod eaque nobis incidunt voluptates unde accusamus et!</p>
+            </div>
+        </div>
+    </article>
+</section>
